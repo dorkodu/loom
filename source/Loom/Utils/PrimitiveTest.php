8b81a1f1d70a649868d1b2d11d68bfdcda868c9a
Doruk <doruk@dorkodu.com>
2020-10-31 11:46:28 +0300

    Developed the Outpost HTTP Library

--- source/Loom/Utils/PrimitiveTest.php
+++ source/Loom/Utils/PrimitiveTest.php
@@ -27,5 +27,22 @@
         return $timer->passedTime();
       } else return false;
     }
+
+    public static function memoryPeak()
+    {
+      return self::formatBytes(memory_get_peak_usage());
+    }
+
+    public static function formatBytes($bytes, $precision = 2)
+    {
+      $units = array("B", "kB", "MB", "GB", "TB");
+  
+      $bytes = max($bytes, 0);
+      $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+      $pow = min($pow, count($units) - 1);
+      $bytes /= (1 << (10 * $pow));
+  
+      return round($bytes, $precision) . " " . $units[$pow];
+    }
   }
   
